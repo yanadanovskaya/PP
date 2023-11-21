02b1e76dfd5c01fb487c8b1bd41d0b2f1bdd5dc9
Yana <91852932+yanadanovskaya@users.noreply.github.com>
2023-11-21 22:56:39 +0300

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,8 @@ import javax.crypto.CipherOutputStream;
 import javax.crypto.spec.SecretKeySpec;
 
 public class Main {
-    public class FileProcessor {
+
+    public static class FileProcessor {
 
         private static final String KEY = "mysecretkey"; // ключ для шифрования
 
@@ -71,7 +72,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String inputFile = "input.txt";//Путь к входному файлу
         String outputFile = "output.txt";//Путь к выходному файлу
 
         try {// Чтение данных из  входного файла
@@ -120,94 +120,51 @@ public class Main {
             double num = Double.parseDouble(tokens[i+1]);
             String operator = tokens[i];
             switch (operator) {
-                case "+":
-                    result += num;
-                    break;
-                case "-":
-                    result -= num;
-                    break;
-                case "*":
-                    result *= num;
-                    break;
-                case "/":
-                    result /= num;
-                    break;
-                default:
-                    throw new IllegalArgumentException("Invalid operator: " + operator);
-            };
+                case "+" -> result += num;
+                case "-" -> result -= num;
+                case "*" -> result *= num;
+                case "/" -> result /= num;
+                default -> throw new IllegalArgumentException("Invalid operator: " + operator);
+            }
         }
 
         return String.valueOf(result);
     }
-    public static class ArithmeticOperationProcessor {
+    private static String processWithoutRegex(String line) {
+        StringBuilder buffer = new StringBuilder();
+        double result = 0;
+        char lastOperator = '+';
 
-        public static void processFileWithoutRegex(String inputFile, String outputFile) {
-            try (BufferedReader reader = new BufferedReader(new FileReader(inputFile));
-                 BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile))) {
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    String processedLine = processLineWithoutRegex(line);
-                    writer.write(processedLine);
-                    writer.newLine();
+        for (int i = 0; i < line.length(); i++) {
+            char c = line.charAt(i);
+            if (Character.isDigit(c)) {
+                buffer.append(c);
+            } else if (isOperator(c)) {
+                double num = Double.parseDouble(buffer.toString());
+                switch (lastOperator) {
+                    case '+' -> result += num;
+                    case '-' -> result -= num;
+                    case '*' -> result *= num;
+                    case '/' -> result /= num;
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
+                buffer.setLength(0);
+                lastOperator = c;
             }
         }
 
-        private static String processLineWithoutRegex(String line) {
-            StringBuilder processedLine = new StringBuilder();
-            int startIndex = 0;
-            int endIndex = 0;
-
-            while (endIndex < line.length()) {
-                char currentChar = line.charAt(endIndex);
-                if (isArithmeticOperator(currentChar)) {
-                    processedLine.append(line, startIndex, endIndex);
-                    // Find the end index of the arithmetic operation
-                    int operationEndIndex = endIndex + 1;
-                    while (operationEndIndex < line.length() && Character.isDigit(line.charAt(operationEndIndex))) {
-                        operationEndIndex++;
-                    }
-
-                    // Evaluate and append the result
-                    String operation = line.substring(endIndex, operationEndIndex);
-                    int result = evaluateArithmeticOperation(operation);
-                    processedLine.append(result);
-
-                    startIndex = operationEndIndex;
-                    endIndex = operationEndIndex;
-                } else {
-                    endIndex++;
-                }
-            }
-
-            processedLine.append(line, startIndex, endIndex);
-            return processedLine.toString();
+        double num = Double.parseDouble(buffer.toString());
+        switch (lastOperator) {
+            case '+' -> result += num;
+            case '-' -> result -= num;
+            case '*' -> result *= num;
+            case '/' -> result /= num;
         }
 
-        private static boolean isArithmeticOperator(char c) {
-            return c == '+';
-        }
-
-        private static int evaluateArithmeticOperation(String operation) {
-            String[] operands = operation.split("[+\\-*/]");
-            int operand1 = Integer.parseInt(operands[0]);
-            int operand2 = Integer.parseInt(operands[1]);
-
-            if (operation.contains("+")) {
-                return operand1 + operand2;
-            } else if (operation.contains("-")) {
-                return operand1 - operand2;
-            } else if (operation.contains("*")) {
-                return operand1 * operand2;
-            } else if (operation.contains("/")) {
-                return operand1 / operand2;
-            }
-
-            throw new IllegalArgumentException("Invalid arithmetic operation: " + operation);
-        }
+        return String.valueOf(result);
     }
 
+    private static boolean isOperator(char c) {
+        return c == '+' || c == '-' || c == '*' || c == '/';
+    }
 
 }
